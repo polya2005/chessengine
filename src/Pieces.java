f3ee4e222b28410efd4f99240ac55e4e5757cd49
polya2005 <65003790+polya2005@users.noreply.github.com>
2023-11-24 13:27:34 -0500

    initial commit

---
+++ src/Pieces.java
@@ -0,0 +1,18 @@
+package chessengine.src;
+
+/**
+ * Constants for piece ids
+ * e.g. white king = Pieces.WHITE | Pieces.KING
+ */
+public class Pieces {
+    public final int PAWN = 0;
+    public final int KNIGHT = 1;
+    public final int BISHOP = 2;
+    public final int ROOK = 3;
+    public final int QUEEN = 4;
+    public final int KING = 5;
+    public final int ALL = 6;
+
+    public final int WHITE = 0;
+    public final int BLACK = 8;
+}
