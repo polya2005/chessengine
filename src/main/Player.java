a527c5c70b6b22bbaa205d7216a0953a9f0481f2
polya2005 <65003790+polya2005@users.noreply.github.com>
2023-11-29 13:19:58 -0500

    Add compatibility with Pieces and Flags

--- src/main/Player.java
+++ src/main/Player.java
@@ -1,6 +1,19 @@
 package chessengine.src.main;
 
 public enum Player {
-    WHITE,
-    BLACK
+    WHITE((short)0),
+    BLACK((short)1);
+
+    private short playerNo;
+    private Player(short playerNo){
+        this.playerNo = playerNo;
+    }
+
+    public short toPlayerNo(){
+        return playerNo;
+    }
+
+    public int toPieceColor(){
+        return playerNo << 3;
+    } 
 }
