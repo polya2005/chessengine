44ca0aa80de68710d5ede2f7a025ab4bab3df3c4
polya2005 <65003790+polya2005@users.noreply.github.com>
2023-11-29 13:20:39 -0500

    change test case

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,7 +4,7 @@ import chessengine.src.main.Board;
 public class Test {
     public static void main(String[] args) {
         Board board = Board.getBoard();
-        board.loadFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        board.loadFEN("rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2");
         System.out.println(board);
     }
 }
